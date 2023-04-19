7f742370bee9cb6d438e3d0de3ceca5a69378d7f
fanosta <github@nageler.org>
2023-04-19 11:08:15 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -eu
 
+if grep -qi debian  /etc/os-release; then
+  commands=(stow curl nvim foobar)
+  packages=(stow curl neovim baz)
+  pkgs=()
+
+  for i in "${!commands[@]}"; do
+      if ! command -v "${commands[i]}" &> /dev/null; then
+          pkgs+=("${packages[i]}")
+      fi
+  done
+
+  if [ ${#pkgs[@]} -ne 0 ]; then
+      echo "The following packages are not installed: ${pkgs[*]}"
+      sudo apt update
+      sudo apt install "${pkgs[@]}"
+  fi
+fi
+
 stow --restow "$@"
 
 for part in "$@"; do
