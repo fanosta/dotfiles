cea05c74d208d19551c37740dfb220d19b071451
Marcel Nageler <fanosta@users.noreply.github.com>
2023-10-03 14:51:45 +0200

    update ipython config

--- ipython/.ipython/profile_sage/startup/00-sage.py
+++ ipython/.ipython/profile_sage/startup/00-sage.py
@@ -20,8 +20,8 @@ def xddt(sbox: SBox):
     if (hasattr(sbox, '_xddt')):
         return sbox._xddt
 
-    nrows = 1 << sbox.m
-    ncols = 1 << sbox.n
+    nrows = 1 << sbox.input_size()
+    ncols = 1 << sbox.output_size()
 
     res = [[set() for _ in range(ncols)] for _ in range(nrows)]
     res = np.zeros((nrows, ncols), dtype=object)
@@ -35,7 +35,7 @@ def xddt(sbox: SBox):
             sy = sbox[y]
             res[x ^ y, sx ^ sy].add(x)
 
-    sbox._xddt = res
+    setattr(sbox, '_xddt', res)
     return res
 
 
@@ -43,8 +43,8 @@ def yddt(sbox: SBox):
     if (hasattr(sbox, '_yddt')):
         return sbox._yddt
 
-    nrows = 1 << sbox.m
-    ncols = 1 << sbox.n
+    nrows = 1 << sbox.input_size()
+    ncols = 1 << sbox.output_size()
 
     res = [[set() for _ in range(ncols)] for _ in range(nrows)]
     res = np.zeros((nrows, ncols), dtype=object)
@@ -58,11 +58,12 @@ def yddt(sbox: SBox):
             sy = sbox[y]
             res[x ^ y, sx ^ sy].add(sx)
 
-    sbox._yddt = res
+    setattr(sbox, '_yddt', res)
     return res
 
 def differential_spectrum(sbox: SBox):
-    return  Counter(np.sort(sbox.ddt().flatten())[::-1])
+    ddt = np.array(sbox.difference_distribution_table())
+    return  Counter(np.sort(ddt.ravel())[::-1])
 
 
 def print_bitmap(bitmap: np.array, displ = lambda x : '# ' if x else '  '):
@@ -91,11 +92,19 @@ def render_bitmap_log(bitmap: np.array, factor: float):
     return img
 
 
-SBox.xddt = xddt
-SBox.yddt = yddt
+sboxes.DEFAULT = SBox(int(x, 16) for x in "037ed4a9cf18b265")
+sboxes.BAKSHEESH = SBox(int(x, 16) for x in "306DB58ECF924A71")
+sboxes.SPEEDY = SBox([8, 0, 9, 3, 56, 16, 41, 19, 12, 13, 4, 7, 48, 1, 32, 35,
+                      26, 18, 24, 50, 62, 22, 44, 54, 28, 29, 20, 55, 52, 5,
+                      36, 39, 2, 6, 11, 15, 51, 23, 33, 21, 10, 27, 14, 31, 49,
+                      17, 37, 53, 34, 38, 42, 46, 58, 30, 40, 60, 43, 59, 47,
+                      63, 57, 25, 45, 61])
 
-SBox.ddt = lambda s : np.array(s.difference_distribution_table())
-SBox.lat = lambda s : np.array(s.linear_approximation_table())
-SBox.bct = lambda s : np.array(s.boomerang_connectivity_table())
+# SBox.xddt = xddt
+# SBox.yddt = yddt
 
-SBox.differential_spectrum = differential_spectrum
+# SBox.ddt = lambda s : np.array(s.difference_distribution_table())
+# SBox.lat = lambda s : np.array(s.linear_approximation_table())
+# SBox.bct = lambda s : np.array(s.boomerang_connectivity_table())
+
+# SBox.differential_spectrum = differential_spectrum
